96ff8faaccce33222101d6957157a912a69de2cb
navyanidamanuri <89773947+navyanidamanuri@users.noreply.github.com>
2021-09-22 09:35:13 +0200

    added xUnit

---
+++ Sum.Tests/CalicTests.cs
@@ -0,0 +1,21 @@
+using System;
+using Xunit;
+
+namespace Sum.Tests
+{
+    public class CalicTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            //Arrange
+            double expected = 0;
+
+            //Act
+            double result = 0;
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
